bc82a3057eb1fd4f53870c7f01b5262d6e5ad749
TheXiShuai <johndoe@example.com>
2023-08-30 13:15:50 +0200

    second commit

---
+++ models/Experience.model.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require("mongoose");
+
+
+const experienceSchema = new Schema({
+  title: { type: String, required: true },
+  location: { type: String, required: true },
+  experienceType: {["Xtreme", "Cultural", "Gastronomic"]},
+  description: { type: String, required: true },
+  duration: { type: String, required: true },
+  price: { type: Number, required: true },
+  imageUrl: { type: String, required: true },
+  tags: [{ type: String }],
+  ratings: { type: Schema.Types.ObjectId, ref: 'Review'},
+  },
+  {
+    timestamps: true
+  }
+
+);
+
+experienceSchema.virtual("averageRating").get(function () {
+    if (this.ratings.length === 0) {
+      return 0; // Return 0 if there are no reviews
+    }
+  
+    const totalScore = this.ratings.reduce((sum, review) => {
+      return sum + review.score;
+    }, 0);
+  
+    const score = totalScore / this.reviews.length;
+    const scoreDecimal = Math.round(score*10)/10
+    return scoreDecimal
+  });
+
+const Experience = model('Experience', experienceSchema);
+
+module.exports = Experience;
