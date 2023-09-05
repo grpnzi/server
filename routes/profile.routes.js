392fcc628c229e25216cb81bfb656cae8abef3c4
German Panzardi <gerpanza@gmail.com>
2023-09-05 14:23:35 +0200

    porfile edit update

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -1,17 +1,27 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
 
 const User = require("./../models/User.model");
 const { /* isAuthenticated */ } = require("../middleware/jwt.middleware");
 
 // EDIT USER
-router.post("/profile/edit/:userId", /* isAuthenticated */ (req, res) => {
+router.post("/profile/edit/:userId", /* isAuthenticated */(req, res) => {
   const userId = req.params.userId;
-  const { email, password, name } = req.body;
-  User.findByIdAndUpdate(userId, { email, password, name }, {new: true})
+  const { email, img, name } = req.body;
+  User.findByIdAndUpdate(userId, { email, img, name }, { new: true })
     .then((modifiedUser) => {
-      res.json(modifiedUser);
+      const { _id, email, name, img, admin } = modifiedUser;
+      // Create an object that will be set as the token payload
+      const payload = { _id, email, name, img, admin };
+
+      // Create a JSON Web Token and sign it
+      const authToken = jwt.sign(payload, process.env.TOKEN_SECRET, {
+        algorithm: "HS256",
+        expiresIn: "6h",
+      });
+      res.json({authToken, payload});
     })
     .catch((error) => {
       console.error(error);
@@ -20,13 +30,13 @@ router.post("/profile/edit/:userId", /* isAuthenticated */ (req, res) => {
 });
 
 // DELETE USER
-router.post("/profile/delete/:userId", /* isAuthenticated */ (req, res) => {
+router.post("/profile/delete/:userId", /* isAuthenticated */(req, res) => {
   const userId = req.params.userId;
   const { email, password, name } = req.body;
   User.findByIdAndRemove(userId, { email, password, name })
-  .then((deletedUser) => {
-    res.json(deletedUser);
-  })
+    .then((deletedUser) => {
+      res.json(deletedUser);
+    })
     .catch((error) => {
       console.error(error);
       res.send(error, "Error deleting data");
